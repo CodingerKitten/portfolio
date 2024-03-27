e1c08fa1098fc7e4aa20aff2b59ef81b9fcbdc8e
CodingKitty <codingercat@gmail.com>
2024-03-27 15:06:32 +0800

    Fixed Island: ArrowLeft key toggle

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,25 +1,45 @@
-import React from 'react'
-
-const renderContent = {
-    1: (
-        <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">1</h1>
-    ),
-    2:(
-        <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">2</h1>
-    ),
-    3:(
-        <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">3</h1>
-    ),
-    4:(
-        <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">4</h1>
-    ),
-}
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {arrow} from '../assets/icons';
 
 const InfoBox = ({text,link, btnText}) => (
     <div className='info-box'>
-
+        <p>{text}</p>
+        <Link to = {link} className='neo-brutalism-white neo-btn'>
+            {btnText}
+            <img src={arrow} />
+        </Link>
     </div>
 )
+
+const renderContent = {
+    1: (
+        <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+            Hi, I'm awesome! 👋
+        </h1>
+    ),
+    2:(
+        <InfoBox
+            text="Eat, sleep, code, coffee, coffee, code, coffee"
+            link="/"
+            btnText="Look at me"
+        />
+    ),
+    3:(
+        <InfoBox
+            text="coffee, coffee, code, coffee"
+            link="/"
+            btnText="Three"
+        />
+    ),
+    4:(
+        <InfoBox
+            text="Eat, sleep, code, coffee"
+            link="/"
+            btnText="Four"
+        />
+    ),
+}
 
 const HomeInfo = ({currentStage}) => {
   return renderContent[currentStage] || null;
